29ef293e12f45649cde5e04e8144bd4ffc83c52d
Vaibhav Singh <vaibhavsingh100070@gmail.com>
2020-06-26 12:09:58 +0530

    Added custom 404 page , about and contact page

--- mvc/controllers/index.js
+++ mvc/controllers/index.js
@@ -4,18 +4,40 @@ let data = require("../../data");
 const postData = data.postData;
 
 const getHomePage = function(req,res){
-    res.render('index' , {title : "Just Me" , posts : postData});
+    res.render('index' , {title : "Just Me" , posts : postData , active : "index"});
 }
 
 const getBlogPost = function(req,res) {
     let params = req.params;
     let post = postData.find((val) => val.id == params.postid);
+
+    if(!post){
+        res.redirect('/404');
+    }
+
     res.render("post" , {title : post.title , post : post});
 }
 
+const get404 = function(req,res) {
+    res.render('404' , {title : '404 - I couldnot find that page....'});
+}
 
+const redirect404 = function(req , res) {
+    res.redirect("/404");
+}
+
+const getAbout = function(req,res) {
+    res.render("about" , {title : "About Me" , active : "about"});
+}
+const getContact = function(req,res) {
+    res.render("contact" , {title : "Contact" , active : "contact"});
+}
 
 module.exports = {
     getHomePage,
-    getBlogPost
+    getBlogPost,
+    get404,
+    redirect404,
+    getAbout,
+    getContact
 }
--- mvc/routes/index.js
+++ mvc/routes/index.js
@@ -6,7 +6,13 @@ const indexController = require("../controllers/index");
 
 /* GET home page. */
 router.get('/', indexController.getHomePage);
-
 router.get('/posts/:postid' , indexController.getBlogPost);
+
+
+router.get("/about" , indexController.getAbout);
+router.get("/contact" , indexController.getContact);
+
+router.get('/404' , indexController.get404);
+router.get("*" , indexController.redirect404);
 
 module.exports = router;
